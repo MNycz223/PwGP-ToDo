93a99e5aec7377bc876cb95c4c7b09dc9f6e7a4d
Krzysztof Nalborczyk <knalborczyk2003@gmail.com>
2026-01-27 14:04:36 +0100

    Extended AuthService with registration and session storage

--- client/AuthService.cs
+++ client/AuthService.cs
@@ -1,15 +1,22 @@
 using System.Net.Http.Headers;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
+using Blazored.SessionStorage;
 
 namespace client;
 
 public class AuthService
 {
     private readonly HttpClient _client;
+    private readonly ISessionStorageService _sessionStorage;
 
-    public AuthService(HttpClient httpClient)
+    private const string AuthTokenKey = "authToken";
+
+    public AuthService(HttpClient httpClient, ISessionStorageService sessionStorage)
     {
         _client = httpClient;
+        _sessionStorage = sessionStorage;
     }
 
     public async Task<bool> LoginAsync(string username, string password)
@@ -19,6 +26,46 @@ public class AuthService
 
         var response = await _client.GetAsync("/login");
 
+        if (response.IsSuccessStatusCode)
+        {
+            await _sessionStorage.SetItemAsync(AuthTokenKey, authHeader);
+            await _sessionStorage.SetItemAsync("username", username);
+            return true;
+        }
+
+        await LogoutAsync();
+        return false;
+    }
+
+    public async Task<bool> RegisterAsync(string username, string password)
+    {
+        var passwordHash = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+        string json = JsonSerializer.Serialize(new { username = username, password = passwordHash });
+        StringContent query = new(json, Encoding.UTF8, "application/json");
+
+        var response = await _client.PostAsync($"/user", query);
         return response.IsSuccessStatusCode;
+    }
+
+    public async Task RestoreLoginAsync()
+    {
+        var token = await _sessionStorage.GetItemAsync<string>(AuthTokenKey);
+
+        if (!string.IsNullOrWhiteSpace(token))
+        {
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", token);
+        }
+    }
+
+    public async Task<bool> IsLoggedAsync()
+    {
+        var token = await _sessionStorage.GetItemAsync<string>(AuthTokenKey);
+        return !string.IsNullOrWhiteSpace(token);
+    }
+
+    public async Task LogoutAsync()
+    {
+        _client.DefaultRequestHeaders.Authorization = null;
+        await _sessionStorage.ClearAsync();
     }
 }
--- client/Program.cs
+++ client/Program.cs
@@ -1,13 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using client;
+using Blazored.SessionStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5282/") });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredSessionStorage();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5041") });
 builder.Services.AddScoped<AuthService>();
 
 await builder.Build().RunAsync();
